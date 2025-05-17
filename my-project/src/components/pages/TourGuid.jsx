4e7fff1762053c2b02bc17ec908c91e13b9e8f4f
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-17 12:50:14 +0600

    tour guid page work done successfully

--- my-project/src/components/pages/TourGuid.jsx
+++ my-project/src/components/pages/TourGuid.jsx
@@ -3,11 +3,21 @@ import DetailsNavber from '../DetailsNavber';
 import detailsBanner from '../../assets/image/hill.jpg'
 import { useLoaderData } from 'react-router-dom';
 import Card from '../Card';
+import { useState } from 'react';
 
 const TourGuid = () => {
 
     const data = useLoaderData();
     console.log(data);
+
+    const [tour, setTour]=useState(data)
+
+    const handleSort=sortBy=>{
+        if(sortBy === 'rating'){
+            const sortByRating=[...data].sort((a,b)=>b.rating -a.rating);
+            setTour(sortByRating);
+        }
+    }
 
 
     return (
@@ -25,27 +35,32 @@ const TourGuid = () => {
             </div>
 
             <div className='w-11/12 mx-auto'>
-                <h2 className='text-4xl font-bold text-center py-6'>{data.length} tour found</h2>
+                <h2 className='text-4xl font-bold text-center py-6'>{data.length} tours found</h2>
 
-                <div className='flex justify-between '>
+                <div className='flex justify-between gap-3'>
 
-                    <section className='col-span-3'>
-                        <div className='space-y-2 border-2 border-black p-3'>
+                    <section className='col-span-3 space-y-4'>
+                        <div className='space-y-2 border-2 border-dash p-3'>
                             <h2 className='font-bold'>Search By Name</h2>
                             <div className="mt-3 border-2 border-dashed rounded-md px-3 py-2 flex items-center">
                                 <input
                                     type="text"
-                                    placeholder="e.g Deluxe bus"
-                                    className="input input-ghost w-full focus:outline-none p-0"
+                                    placeholder="search by name"
+                                    className="text-xl input input-ghost w-full focus:outline-none p-0"
                                 />
                             </div>
                         </div>
+                        <div className='space-y-2 border-2 border-dash p-3'>
+                            
+                            <button onClick={()=>handleSort('rating')} className='btn bg-purple-500 text-white'>Filter By Rating</button>
+                            
+                        </div>
                     </section>
 
-                    <section className='col-span-9 grid grid-cols-1 md:grid-cols-3'>
+                    <section className='col-span-9 grid grid-cols-1 md:grid-cols-3 gap-3'>
                         {
 
-                            data.map(tour => <Card hotel={tour}></Card>)
+                            tour.map(tour => <Card hotel={tour}></Card>)
 
                         }
                     </section>
