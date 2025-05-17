6136cc7fd0e92ba6fc5c5c7d2d35ca6c1ddaf6ac
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-17 12:32:01 +0600

    tour guid page work in 50%

--- my-project/src/components/pages/TourGuid.jsx
+++ my-project/src/components/pages/TourGuid.jsx
@@ -24,13 +24,32 @@ const TourGuid = () => {
 
             </div>
 
-            <div className='flex justify-between '>
-                <section className='col-span-3'>jhgjhgjh</section>
-                <section className='col-span-9 grid grid-cols-1 md:grid-cols-3'>
-                    {
-                        data.map(tour => <Card hotel={tour}></Card>)
-                    }
-                </section>
+            <div className='w-11/12 mx-auto'>
+                <h2 className='text-4xl font-bold text-center py-6'>{data.length} tour found</h2>
+
+                <div className='flex justify-between '>
+
+                    <section className='col-span-3'>
+                        <div className='space-y-2 border-2 border-black p-3'>
+                            <h2 className='font-bold'>Search By Name</h2>
+                            <div className="mt-3 border-2 border-dashed rounded-md px-3 py-2 flex items-center">
+                                <input
+                                    type="text"
+                                    placeholder="e.g Deluxe bus"
+                                    className="input input-ghost w-full focus:outline-none p-0"
+                                />
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className='col-span-9 grid grid-cols-1 md:grid-cols-3'>
+                        {
+
+                            data.map(tour => <Card hotel={tour}></Card>)
+
+                        }
+                    </section>
+                </div>
             </div>
 
         </div>
